8e996deb902c2c4f08b842abbc67546133c0b317
DESKTOP-I8DK67B\Riton <ritonelion@outlook.com>
2022-06-19 23:51:27 +0800

    base copy

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,80 @@
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
-use std::fs::{create_dir_all, File};
+use std::fs;
+use std::fs::{create_dir, create_dir_all, File, read_to_string};
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-static THRESHOLD: u32 = u32::MAX / 100 * 20;
-static MAX_LEVEL: u32 = 5;
+fn copy_file(from : &Path, to: &Path) {
+    let content = fs::read_to_string(from).unwrap();
+    let mut file = fs::File::create(to).unwrap();
+    file.write_all(content.as_bytes()).unwrap();
+}
 
-fn if_continue_gen(level: u32) -> bool {
-    let i: u32 = thread_rng().gen();
-    //the lesser the level is,  the return tends to true
-    match i.checked_mul(MAX_LEVEL - level) {
-        Some(r) => r > THRESHOLD,
-        None => true,
+fn copy_dir(to: &Path) {
+    create_dir(to).unwrap();
+}
+
+#[cfg(test)]
+mod copy_test {
+    use super::*;
+
+    #[test]
+    fn copy_file_test() {
+        let from = Path::new("./test_dir/copy_test_dir/origin_file");
+        let to = Path::new("./test_dir/copy_test_dir/copied_file1");
+        copy_file(&from, &to);
+        let content_from = fs::read_to_string(&from).unwrap();
+        let content_to = fs::read_to_string(&to).unwrap();
+        assert_eq!(content_to, content_from);
     }
 }
 
-fn if_continue_gen_file() -> bool {
-    let i: u32 = thread_rng().gen();
-    i > THRESHOLD
-}
-
-fn gen_string(len: usize) -> String {
-    thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(len)
-        .map(char::from)
-        .collect()
-}
-
-fn gen_dir_under_path(upper_path: &str, mut level: u32) {
-    level += 1;
-    let mut i = 0;
-
-    while if_continue_gen_file() {
-        println!("gen file");
-        let file_name = gen_string(6);
-        let upper = upper_path.to_string();
-        let full_path = upper + "/" + &file_name + &i.to_string();
-        let mut file = File::create(Path::new(&full_path)).unwrap();
-        file.write_all(gen_string(100).as_bytes()).unwrap();
-        i += 1;
-    }
-
-    if level >= MAX_LEVEL {
-        return;
-    }
-
-    while if_continue_gen(level) {
-        println!("gen dir");
-        let dir_name = gen_string(6);
-        let upper = upper_path.to_string();
-        let full_path = upper + "/" + &dir_name + &i.to_string();
-        let new_path_prefix = Path::new(&full_path);
-        create_dir_all(new_path_prefix).unwrap();
-        gen_dir_under_path(&full_path, level);
-        i += 1;
+fn vist_dirs(dir: &Path) {
+    for entry in fs::read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        println!("parent is {}", entry.path().parent().unwrap().to_str().unwrap());
+        if path.is_dir() {
+            vist_dirs(&path);
+        } else {
+            println!("{}", entry.path().to_str().unwrap());
+        }
     }
 }
 
-fn create_test_dir() {
-    let origin_path = "./test_dir/origin";
-    create_dir_all(Path::new(origin_path)).unwrap();
-    gen_dir_under_path(origin_path, 0);
+fn copy_dir_recursive(from: &Path, dest: &Path, depth_path: &PathBuf) {
+    let read_dir = from.clone().to_path_buf().join(depth_path);
+    println!("-----------");
+    println!("from : {:?}", from);
+    println!("dest : {:?}", dest);
+    println!("depth_path : {:?}", depth_path);
+    println!("read_dir : {:?}", read_dir);
+    for entry in fs::read_dir(read_dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_dir() {
+            let new_depth_path =
+                depth_path.clone()
+                    .join(Path::new(&entry.file_name()));
+            println!("next depth's path: {:?}", new_depth_path);
+            println!("creating path: {:?}", new_depth_path);
+            let creating_path = dest.clone().to_path_buf().join(new_depth_path.clone());
+            create_dir_all(&creating_path).unwrap();
+            copy_dir_recursive(from, dest, &new_depth_path);
+        } else {
+            let new_depth_path =
+                depth_path.clone()
+                    .join(Path::new(&entry.file_name()));
+            let creating_file = dest.clone().to_path_buf().join(new_depth_path.clone());
+            let read_file = from.clone().to_path_buf().join(new_depth_path.clone());
+            println!("creating file : {:?}", creating_file);
+            let mut file = File::create(creating_file).unwrap();
+            let content = read_to_string(read_file).unwrap();
+            println!("copying content");
+            file.write_all(content.as_bytes()).unwrap();
+        }
+    }
 }
 
 fn main() {
-    create_test_dir();
+    copy_dir_recursive(Path::new("./test_dir/copy_test_dir"), Path::new("./test_dir/copy_test_dir_copied"), &PathBuf::new());
 }
---
+++ src/test_gen.rs
@@ -0,0 +1,70 @@
+use std::fs::{create_dir_all, File};
+use std::io::Write;
+use std::path::Path;
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
+
+static THRESHOLD: u32 = u32::MAX / 100 * 20;
+static MAX_LEVEL: u32 = 5;
+
+fn if_continue_gen(level: u32) -> bool {
+    let i: u32 = thread_rng().gen();
+    //the lesser the level is,  the return tends to true
+    match i.checked_mul(MAX_LEVEL - level) {
+        Some(r) => r > THRESHOLD,
+        None => true,
+    }
+}
+
+fn if_continue_gen_file() -> bool {
+    let i: u32 = thread_rng().gen();
+    i > THRESHOLD
+}
+
+fn gen_string(len: usize) -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(char::from)
+        .collect()
+}
+
+fn gen_dir_under_path(upper_path: &str, mut level: u32) {
+    level += 1;
+    let mut i = 0;
+
+    while if_continue_gen_file() {
+        println!("gen file");
+        let file_name = gen_string(6);
+        let upper = upper_path.to_string();
+        let full_path = upper + "/" + &file_name + &i.to_string();
+        let mut file = File::create(Path::new(&full_path)).unwrap();
+        file.write_all(gen_string(100).as_bytes()).unwrap();
+        i += 1;
+    }
+
+    if level >= MAX_LEVEL {
+        return;
+    }
+
+    while if_continue_gen(level) {
+        println!("gen dir");
+        let dir_name = gen_string(6);
+        let upper = upper_path.to_string();
+        let full_path = upper + "/" + &dir_name + &i.to_string();
+        let new_path_prefix = Path::new(&full_path);
+        create_dir_all(new_path_prefix).unwrap();
+        gen_dir_under_path(&full_path, level);
+        i += 1;
+    }
+}
+
+fn create_test_dir() {
+    let origin_path = "./test_dir/origin";
+    create_dir_all(Path::new(origin_path)).unwrap();
+    gen_dir_under_path(origin_path, 0);
+}
+
+fn main() {
+    create_test_dir();
+}
