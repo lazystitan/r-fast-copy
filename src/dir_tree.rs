98d69a04e15e5acbd941c77dd5ae7cb5173859fb
DESKTOP-I8DK67B\Riton <ritonelion@outlook.com>
2022-06-22 21:33:52 +0800

    some opt

--- src/dir_tree.rs
+++ src/dir_tree.rs
@@ -2,34 +2,48 @@ use std::cell::{Cell, RefCell};
 use std::path::PathBuf;
 use std::rc::Rc;
 
+pub type SharedNodeRef = Rc<RefCell<DirNode>>;
+
 #[derive(Debug)] //TODO overflowed
 pub struct DirNode {
-    parent: Option<Rc<RefCell<DirNode>>>,
-    path: PathBuf,
-    is_copied: bool,
-    sub_nodes: Vec<Rc<RefCell<DirNode>>>
+    _parent: Option<SharedNodeRef>,
+    _path: PathBuf,
+    _is_copied: bool,
+    _sub_nodes: Vec<SharedNodeRef>
 }
 
 impl DirNode {
     fn new(path: PathBuf) -> Self {
         Self {
-            parent: None,
-            path,
-            is_copied: false,
-            sub_nodes: Vec::new()
+            _parent: None,
+            _path: path,
+            _is_copied: false,
+            _sub_nodes: Vec::new()
         }
     }
 
-    fn add_sub_nodes(&mut self, node: Rc<RefCell<DirNode>>) {
-        self.sub_nodes.push(node);
+    fn add_sub_nodes(&mut self, node: SharedNodeRef) {
+        self._sub_nodes.push(node);
     }
 
     fn copied(&mut self) {
-        self.is_copied = true;
+        self._is_copied = true;
     }
 
     fn is_copied(&self) -> bool {
-        return self.is_copied;
+        return self._is_copied;
+    }
+
+    fn set_parent(&mut self, p: SharedNodeRef) {
+        self._parent = Some(p);
+    }
+
+    fn sub_nodes(&self) -> Vec<SharedNodeRef> {
+        self._sub_nodes.iter().map(|x| x.clone()).collect()
+    }
+
+    fn path(&self) -> &PathBuf {
+        &self._path
     }
 }
 
@@ -48,13 +62,13 @@ mod test {
         for i in 0..5 {
             let p = PathBuf::from("./test_dir/tree/c".to_string() + &i.to_string());
             let mut tn = DirNode::new(p);
-            tn.parent = Some(root_rc.clone());
+            tn.set_parent(root_rc.clone());
             root_rc.borrow_mut().add_sub_nodes(Rc::new(RefCell::new(tn)));
         }
-        for i in &root_rc.borrow().sub_nodes {
-            println!("sub node {:?}", i.borrow().path);
+        for i in &root_rc.borrow().sub_nodes() {
+            println!("sub node {:?}", i.borrow().path());
         }
-        println!("{:?}", root_rc);
+        // println!("{:?}", root_rc);
     }
 
     #[test]
